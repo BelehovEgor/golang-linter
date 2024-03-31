f6ba363653bed9991702e9b8a9110b13a8910ac3
ebelekhov <ebelekhov@ozon.ru>
2024-03-31 12:55:46 +0300

    added simple setup of lint with uber settings

---
+++ go.mod
@@ -0,0 +1,3 @@
+module golang-linter
+
+go 1.22
---
+++ simpleApp.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+
+}
+
+func wrong_name_style(b bool) int {
+	var a int
+	if b {
+		a = 100
+	} else {
+		a = 10
+	}
+
+	return a
+}
