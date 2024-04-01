2d1cc67c3e1ae6b55454ee25a7726783bf9e22f1
kechin.vv <kechinv1@rambler.ru>
2024-04-01 10:39:13 +0300

    ungrouped elements (types)

--- simpleApp.go
+++ simpleApp.go
@@ -8,13 +8,19 @@ func wrong_name_style(b bool) int {
 	var a int
 	var c = 2
 	var d = 3
+	area := Area(2.1)
+	vol := Volume(3.1)
 	if b {
+		area += area
+		vol += vol
 		c++
 		a = 100
 	} else {
 		d--
 		a = 10
 	}
-
 	return a
 }
+
+type Area float64
+type Volume float64
